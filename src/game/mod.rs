fbb0494c52c579f211c89beb632f442197316dff
Juan Carlos <juanksoliveira@gmail.com>
2021-02-01 02:55:46 -0300

    feat: setup input events

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -6,6 +6,7 @@ use piston::EventSettings;
 use piston::Events;
 use piston_window::clear;
 use piston_window::rectangle;
+use piston_window::Button;
 use piston_window::PistonWindow;
 use piston_window::WindowSettings;
 use sdl2_window::Sdl2Window;
@@ -34,6 +35,26 @@ impl Game {
     pub fn render(&mut self) {
         let mut events = Events::new(EventSettings::new());
         while let Some(event) = events.next(&mut self.window) {
+            match piston::PressEvent::press_args(&event) {
+                Some(Button::Keyboard(piston::Key::Up)) => println!("UP"),
+                Some(Button::Keyboard(piston::Key::Right)) => println!("RIGHT"),
+                Some(Button::Keyboard(piston::Key::Down)) => println!("DOWN"),
+                Some(Button::Keyboard(piston::Key::Left)) => println!("LEFT"),
+                Some(Button::Mouse(piston::MouseButton::Left)) => {
+                    println!("Pressed Left Mouse button")
+                }
+                Some(Button::Mouse(piston::MouseButton::Right)) => {
+                    println!("Pressed Right Mouse button");
+                }
+                Some(Button::Keyboard(key)) => println!("Pressed Key: {:#?}.", key),
+                _ => {}
+            }
+
+            event.mouse_cursor(|x| {
+                println!("MouseX: {}", x[0]);
+                println!("MouseY: {}", x[1]);
+            });
+
             self.window.draw_2d(&event, |c, g, _| {
                 clear([0.5, 0.5, 0.5, 1.0], g);
                 rectangle(
